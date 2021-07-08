291e34557ceef7c0a7c0fda016679787e59f8fcc
BIHRI BABU <79754454+mafiapikachu@users.noreply.github.com>
2021-07-08 10:57:19 +0530

    Create string.sh

---
+++ string.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#   Copyright (C) 2021 mafiapikachu
+#
+#   Poject MORPHOBOT is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+                      © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_Morphobot™
+           .___  ___.   ______   .______      .______    __    __    ______       __    __       _______. _______ .______      .______     ______   .___________.
+|   \/   |  /  __  \  |   _  \     |   _  \  |  |  |  |  /  __  \     |  |  |  |     /       ||   ____||   _  \     |   _  \   /  __  \  |           |
+|  \  /  | |  |  |  | |  |_)  |    |  |_)  | |  |__|  | |  |  |  |    |  |  |  |    |   (----`|  |__   |  |_)  |    |  |_)  | |  |  |  | `---|  |----`
+|  |\/|  | |  |  |  | |      /     |   ___/  |   __   | |  |  |  |    |  |  |  |     \   \    |   __|  |      /     |   _  <  |  |  |  |     |  |     
+|  |  |  | |  `--'  | |  |\  \----.|  |      |  |  |  | |  `--'  |    |  `--'  | .----)   |   |  |____ |  |\  \----.|  |_)  | |  `--'  |     |  |     
+|__|  |__|  \______/  | _| `._____|| _|      |__|  |__|  \______/      \______/  |_______/    |_______|| _| `._____||______/   \______/      |__|     
+                                                                                                                                                      
+                     © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_MORPHOBOT™                       
+                         @morpho_userbot
+"""
+
+python3 string_session.py
